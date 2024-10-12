e9b8b39a3d7aa83dd93f2714da7cf3543a0aadc8
subham0269 <gitsubham21@gmail.com>
2024-10-12 10:30:44 +0530

    Add new posters and styling changes

--- src/Components/MyWork.jsx
+++ src/Components/MyWork.jsx
@@ -1,5 +1,7 @@
 import RajaRaniRomeo from '../assets/Work/raja-rani-romeo.png';
 import Meyebela from '../assets/Work/meyebela.png';
+import khelaghar from '../assets/Work/khelaghor.png';
+import AlorKole from '../assets/Work/alor_kole.png';
 
 
 const MyWork = () => {
@@ -14,11 +16,11 @@ const MyWork = () => {
             title: 'Meyebela'
         },
         {
-            img: RajaRaniRomeo,
+            img: khelaghar,
             title: 'Khelaghar'
         },
         {
-            img: Meyebela,
+            img: AlorKole,
             title: 'Alor Kole'
         }
     ]
@@ -28,8 +30,10 @@ const MyWork = () => {
             <h2 className="text-center font-semibold text-3xl sm:text-4xl text-[#000000D6]">My Work</h2>
             <div className='flex flex-col md:flex-row space-y-7 md:space-y-0 md:flex-wrap sm:justify-center xl:justify-between items-center md:gap-12 xl:gap-0'>
                 {workList.map((e,i) => (
-                    <div key={i}>
-                        <img className='w-[90%] lg:w-auto h-auto mx-auto' src={e.img} alt={`poster-${e.title}`}/>
+                    <div className='grow flex flex-col justify-between' key={i}>
+                        <div className=''>
+                            <img className='w-[90%]  lg:max-w-[350px] h-auto aspect-[3/4] mx-auto object-cover object-center' src={e.img} alt={`poster-${e.title}`}/>
+                        </div>
                         <h3 className='text-center mt-2'>{e.title}</h3>
                     </div>
                 ))}
